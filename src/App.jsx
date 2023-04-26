d61b04b20eff709d9380abd75585dc9035c5b54a
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2023-04-26 17:00:09 -0400

    init commt

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to best webapp in the world</h1>
+        <h2>where you can find anything you want</h2>
+      </header>
+    </div>
+  )
+}
+
+export default App
