5f91c10c804ddae24cad29bdc33d58322055d1af
marlondenisck <15050029+marlondenisck@users.noreply.github.com>
2023-04-26 17:32:10 -0400

    Detectar automaticamente linguagem do browser

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,11 +1,15 @@
 import i18n from "i18next";
+import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from "react-i18next";
 
 import enJson from  './translations/en.json'
 import ptBRJson from  './translations/pt-br.json'
-console.log('rodou')
-i18n.use(initReactI18next).init({
-  fallbackLng: "ptBr", // default language
+
+i18n
+.use(LanguageDetector) // Usa o detector de idioma do seu browser
+.use(initReactI18next)
+.init({
+  fallbackLng: "en", // default language
   interpolation: {
     escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
   },
